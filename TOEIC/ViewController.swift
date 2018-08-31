8b812436e5d92e2db098d496ff26e8d46c29926c
Thieu Mao <thieumao@gmail.com>
2018-08-31 19:08:38 +0700

    Init Project

---
+++ TOEIC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TOEIC
+//
+//  Created by Thieu Mao on 8/31/18.
+//  Copyright © 2018 Thieu Mao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
